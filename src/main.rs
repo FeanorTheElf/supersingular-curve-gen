d0bc8cdbec5ebf3196b0ed3eb0b1f9af0f0125ec
Simon Pohmann <simon@pohmann.de>
2022-05-11 13:10:01 +0100

    Merged

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 
 extern crate feanor_la;
 
-use feanor_la::ring::*;
-use feanor_la::primitive::*;
+use feanor_la::prelude::*;
+use feanor_la::wrapper::*;
 use feanor_la::algebra::elliptic_curve::*;
 use feanor_la::algebra::poly::*;
 use feanor_la::algebra::fractions::*;
-use feanor_la::embedding::*;
-use feanor_la::wrapper::*;
 
 fn main() {
     let mut R = MultivariatePolyRing::new(i64::RING);
@@ -30,6 +28,10 @@ fn main() {
     let P1 = EllipticCurvePoint::<&WrappingRing<_>>::Affine(x1/&z1, y1/z1);
     let P2 = EllipticCurvePoint::<&WrappingRing<_>>::Affine(x2/&z2, y2/z2);
     println!("{:?}", E.point_add(P1, P2));
+<<<<<<< Updated upstream
     // ((y2^2z1^5z2^3 + -2 * y1y2z1^4z2^4 + y1^2z1^3z2^5 + -1 * x2^3z1^5z2^2 + x1x2^2z1^4z2^3 + x1^2x2z1^3z2^4 + -1 * x1^3z1^2z2^5) / (x2^2z1^5z2^3 + -2 * x1x2z1^4z2^4 + x1^2z1^3z2^5), 
     // (y2^3z1^9z2^4 + -3 * y1y2^2z1^8z2^5 + 3 * y1^2y2z1^7z2^6 + -1 * y1^3z1^6z2^7 + -1 * x2^3y2z1^9z2^3 + 2 * x2^3y1z1^8z2^4 + -3 * x1x2^2y1z1^7z2^5 + 3 * x1^2x2y2z1^7z2^5 + -2 * x1^3y2z1^6z2^6 + x1^3y1z1^5z2^7) / (-1 * x2^3z1^9z2^4 + 3 * x1x2^2z1^8z2^5 + -3 * x1^2x2z1^7z2^6 + x1^3z1^6z2^7))
+=======
+    // ((y2^2z1^5z2^3 + -2 * y1y2z1^4z2^4 + y1^2z1^3z2^5 + -1 * x2^3z1^5z2^2 + x1x2^2z1^4z2^3 + x1^2x2z1^3z2^4 + -1 * x1^3z1^2z2^5) / (x2^2z1^5z2^3 + -2 * x1x2z1^4z2^4 + x1^2z1^3z2^5), (y2^3z1^9z2^4 + -3 * y1y2^2z1^8z2^5 + 3 * y1^2y2z1^7z2^6 + -1 * y1^3z1^6z2^7 + -1 * x2^3y2z1^9z2^3 + 2 * x2^3y1z1^8z2^4 + -3 * x1x2^2y1z1^7z2^5 + 3 * x1^2x2y2z1^7z2^5 + -2 * x1^3y2z1^6z2^6 + x1^3y1z1^5z2^7) / (-1 * x2^3z1^9z2^4 + 3 * x1x2^2z1^8z2^5 + -3 * x1^2x2z1^7z2^6 + x1^3z1^6z2^7))
+>>>>>>> Stashed changes
 }
